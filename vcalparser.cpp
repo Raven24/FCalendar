33403011a6a2c27d3f5edf9efa1ae5a987c52096
Florian Staudacher <florian_staudacher@yahoo.de>
2010-11-03 13:37:00 +0000

    improved performance for reading ical files a bit

--- vcalparser.cpp
+++ vcalparser.cpp
@@ -27,56 +27,63 @@ QList<QMap <QString, QString> > VCalParser::split()
 
 QList<QMap <QString, QString> > VCalParser::split(QString type)
 {
+	//QTime time;
 	QStringList lines = m_rawData->split(QRegExp("\n"), QString::SkipEmptyParts);
-
+	QStringList::const_iterator i;
 	QList<QMap <QString, QString> > itemsList;
 	QMap<QString, QString> temp;
 
-	for (int i = 0; i < lines.size(); ++i) {
-		if (lines.at(i).contains("BEGIN:" + type)) {
+	//time.start();
+	for (i = lines.constBegin(); i != lines.constEnd(); ++i) {
+		if ((*i).contains("BEGIN:" + type)) {
 			// make a clean list for the new event
 			temp.clear();
 		}
-		if(!lines.at(i).startsWith(" ") && lines.at(i).contains(":")) {
-			temp[lines.at(i).split(":").at(0)] = lines.at(i).split(":").at(1);
+		if(!(*i).startsWith(" ") && (*i).contains(":")) {
+			temp[(*i).split(":").at(0)] = (*i).split(":").at(1);
 		}
-		if (lines.at(i).contains("END:" + type)) {
+		if ((*i).contains("END:" + type)) {
 			// event is over, append to itemsList
 			itemsList.append(temp);
 		}
 
 	}
+	//qDebug() << "[debug] splitting took: " << time.elapsed();
 	return itemsList;
 }
 
 void VCalParser::getEvents()
 {
 	qDebug() << "[calendar] parsing events";
+	//QTime time;
 
 	QList<QMap<QString, QString> > eventList = split("VEVENT");
 	nextEvent.setStart(QDateTime::currentDateTime().addYears(100));
 
-	for (int i = 0; i < eventList.size(); i++) {
+	//time.start();
+	QList<QMap<QString, QString> >::const_iterator i;
+	for (i = eventList.constBegin(); i != eventList.constEnd(); ++i) {
+
 		TEvent event;
-		event.setUid(eventList.at(i)["UID"]);
-		event.setDescription(eventList.at(i)["DESCRIPTION"]);
-		event.setSummary(eventList.at(i)["SUMMARY"]);
-		event.setLocation(eventList.at(i)["LOCATION"]);
+		event.setUid((*i).value("UID"));
+		event.setDescription((*i).value("DESCRIPTION"));
+		event.setSummary((*i).value("SUMMARY"));
+		event.setLocation((*i).value("LOCATION"));
 		// start date
-		if (eventList.at(i)["DTSTART"].size() > 1) {
-			event.setStart(decodeDate(eventList.at(i)["DTSTART"]));
-		} else if (eventList.at(i)["DTSTART;VALUE=DATE"].size() > 1) {
-			event.setStart(decodeDate(eventList.at(i)["DTSTART;VALUE=DATE"]));
+		if ((*i).value("DTSTART").size() > 1) {
+			event.setStart(decodeDate((*i).value("DTSTART")));
+		} else if ((*i).value("DTSTART;VALUE=DATE").size() > 1) {
+			event.setStart(decodeDate((*i).value("DTSTART;VALUE=DATE")));
 		}
 		// end date
-		if (eventList.at(i)["DTEND"].size() > 1) {
-			event.setEnd(decodeDate(eventList.at(i)["DTEND"]));
-		} else if (eventList.at(i)["DTEND;VALUE=DATE"].size() > 1) {
-			event.setEnd(decodeDate(eventList.at(i)["DTEND;VALUE=DATE"]));
+		if ((*i).value("DTEND").size() > 1) {
+			event.setEnd(decodeDate((*i).value("DTEND")));
+		} else if ((*i).value("DTEND;VALUE=DATE").size() > 1) {
+			event.setEnd(decodeDate((*i).value("DTEND;VALUE=DATE")));
 		}
-		event.setLastModified(decodeDate(eventList.at(i)["LAST-MODIFIED"]));
-		event.setClass(eventList.at(i)["CLASS"]);
-		event.setStatus(eventList.at(i)["STATUS"]);
+		event.setLastModified(decodeDate((*i).value("LAST-MODIFIED")));
+		event.setClass((*i).value("CLASS"));
+		event.setStatus((*i).value("STATUS"));
 
 		// event is in the future
 		if(event.getStart() > QDateTime::currentDateTime()) {
@@ -84,9 +91,9 @@ void VCalParser::getEvents()
 				nextEvent = event;
 			}
 		}
-
 		m_events.append(event);
 	}
+	//qDebug() << "[debug] parsing events took: " << time.elapsed();
 
 	qSort(m_events);
 
@@ -103,16 +110,17 @@ void VCalParser::getTodos()
 	qDebug() << "[calendar] parsing todos";
 	QList<QMap<QString, QString> > todoList = split("VTODO");
 
-	for (int i = 0; i < todoList.size(); i++) {
+	QList<QMap<QString, QString> >::const_iterator i;
+	for (i = todoList.constBegin(); i != todoList.constEnd(); ++i) {
 		TTodo todo;
-		todo.setUid(todoList.at(i)["UID"]);
-		todo.setDescription(todoList.at(i)["DESCRIPTION"]);
-		todo.setSummary(todoList.at(i)["SUMMARY"]);
-		todo.setLocation(todoList.at(i)["LOCATION"]);
-		todo.setPercentComplete(todoList.at(i)["PERCENT-COMPLETE"].toInt());
-		todo.setLastModified(decodeDate(todoList.at(i)["LAST-MODIFIED"]));
-		todo.setClass(todoList.at(i)["CLASS"]);
-		todo.setStatus(todoList.at(i)["STATUS"]);
+		todo.setUid((*i).value("UID"));
+		todo.setDescription((*i).value("DESCRIPTION"));
+		todo.setSummary((*i).value("SUMMARY"));
+		todo.setLocation((*i).value("LOCATION"));
+		todo.setPercentComplete((*i).value("PERCENT-COMPLETE").toInt());
+		todo.setLastModified(decodeDate((*i).value("LAST-MODIFIED")));
+		todo.setClass((*i).value("CLASS"));
+		todo.setStatus((*i).value("STATUS"));
 
 		m_todos.append(todo);
 	}
